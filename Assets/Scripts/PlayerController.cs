6ec27eff5212c740df18e198ee0e30617aac728a
Dimasia <kovyazinaa11@yandex.ru>
2026-05-04 05:49:18 +0300

    fix code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,50 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Basics Movements")] public float basicSpeed = 5f;
+    [Header("Basics Movements")] 
+    public float basicSpeed = 5f;
     public float basicSens = 2f;
+
+    [Header("Links")] 
+    public Transform cameraRoot;
+    public AudioSource audioSource;
+
+    private float _ws;
+    private float _ad;
     private float _mouseXRotation;
-
-    [Header("Links")] public Transform cameraRoot;
     private Rigidbody _rigidbodyPlayer;
-
-    void Start()
+    
+    private void Start()
     {
         _rigidbodyPlayer = GetComponent<Rigidbody>();
         _rigidbodyPlayer.freezeRotation = true;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        audioSource.Stop();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        var ws = Input.GetAxis("Vertical");
-        var ad = Input.GetAxis("Horizontal");
+        // ------------- ЗВУК ---------------------------------
+        bool isMoving = _rigidbodyPlayer.linearVelocity.magnitude > 0.1f;
+        
+        if (isMoving && !audioSource.isPlaying)
+            audioSource.Play();
+        else if (!isMoving)
+            audioSource.Stop();
+    }
 
-        var moveInput = transform.forward * ws + transform.right * ad;
+    private void FixedUpdate()
+    {
+        // ------------- Движение Игрока! ---------------------------------
+        _ws = Input.GetAxis("Vertical");
+        _ad = Input.GetAxis("Horizontal");
+        var moveInput = transform.forward * _ws + transform.right * _ad;
 
         if (moveInput.magnitude > 1f) moveInput.Normalize();
-
+        
         _rigidbodyPlayer.linearVelocity = new Vector3(
             moveInput.x * basicSpeed,
             _rigidbodyPlayer.linearVelocity.y,
@@ -37,13 +56,19 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
+        // --------------МЫШЬ---------------------------------
+        // Мышь
         var mouseX = Input.GetAxis("Mouse X") * basicSens;
         var mouseY = Input.GetAxis("Mouse Y") * basicSens;
 
+        // Шампур для башки
         transform.Rotate(Vector3.up * mouseX);
 
+        // Инверсия поворота (МЫ НЕ В ВЕРТОЛ ЕТЕ)
         _mouseXRotation -= mouseY;
+        // Лочим
         _mouseXRotation = Mathf.Clamp(_mouseXRotation, -90f, 90f);
+        // Применяем
         cameraRoot.localRotation = Quaternion.Euler(_mouseXRotation, 0f, 0f);
     }
 }
